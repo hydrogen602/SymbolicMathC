13e67003e2bb284937e20dacef8fa0aee72b087b
Jonathan Rotter <hydrogen31415@gmail.com>
2020-09-09 05:28:00 +0200

    example

---
+++ test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int nextStep(int x) {
+    printf("%d\n", x);
+
+    if (x == 1) {
+        return 1;
+    }
+    else if (x % 2 == 0) {
+        return nextStep(x / 2) + 1;
+    }
+    else {
+        return nextStep(3*x+1) + 1;
+    }
+}
+
+int main() {
+
+    nextStep(5);
+}
