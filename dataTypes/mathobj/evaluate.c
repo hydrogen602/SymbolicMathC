c2eb1becc4add995a4bf3038a141941996f0c4a6
Jonathan Rotter <hydrogen31415@gmail.com>
2020-09-16 22:18:30 +0200

    reorganization

--- dataTypes/mathobj/evaluate.c
+++ dataTypes/mathobj/evaluate.c
@@ -131,34 +131,149 @@ math_obj math_obj_eval_negate(math_obj self) {
     return self;
 }
 
+math_obj math_obj_eval_plus(math_obj self) {
+    int childCount = len(self->children);
+    assert(self->typeTag == PLUS);
+    assert(childCount > 1);
+    // analyze for possible simplification
+    math_obj last = NULL;
+    int indexOfFirst = -1;
+
+    for (int i = 0; i < childCount; ++i) {
+        if (math_obj_isConstant(self->children[i])) {
+            if (last == NULL) {
+                last = self->children[i];
+                self->children[i] = NULL;
+                indexOfFirst = i;
+            }
+            else {
+                math_obj newlast = __math_obj_eval_plus(last, self->children[i]);
+                math_obj_free(last);
+                math_obj_free(self->children[i]);
+                self->children[i] = NULL;
+
+                last = newlast;
+            }
+        }
+    }
+
+    if (last != NULL) {
+        assert(math_obj_isConstant(last));
+        long int val = math_obj_mvalue_getAsLong(last);
+        if (val == 0) {
+            math_obj_free(last);
+            self->children[indexOfFirst] = NULL;
+        }
+        else {
+            self->children[indexOfFirst] = last;
+        }
+        
+    }
+
+    self->children = __math_obj_takeOutNull(self->children);
+
+    if (len(self->children) == 1) {
+        // only one left
+        math_obj tmp = self->children[0];
+        self->children[0] = NULL;
+        math_obj_free(self);
+        return tmp;
+    }
+    else if (len(self->children) == 0) {
+        // nothing left
+        math_obj tmp = buildMathObjectConstantLong(0);
+        math_obj_free(self);
+        return tmp;
+    }
+
+    return self;
+}
+
+math_obj math_obj_eval_product(math_obj self) {
+    int childCount = len(self->children);
+
+    assert(childCount > 1);
+    // analyze for possible simplification
+    math_obj last = NULL;
+    int indexOfFirst = -1;
+
+    for (int i = 0; i < childCount; ++i) {
+        if (math_obj_isConstant(self->children[i])) {
+
+            long int val = math_obj_mvalue_getAsLong(self->children[i]);
+            
+            if (val == 0) {
+                // everything is zero
+                math_obj_free(self);
+                return buildMathObjectConstantLong(0);
+            }
+
+            if (last == NULL) {
+                last = self->children[i];
+                self->children[i] = NULL;
+                indexOfFirst = i;
+            }
+            else {
+                math_obj newlast = __math_obj_eval_product(last, self->children[i]);
+                math_obj_free(last);
+                math_obj_free(self->children[i]);
+                self->children[i] = NULL;
+
+                last = newlast;
+            }
+        }
+    }
+
+    if (last != NULL) {
+        assert(math_obj_isConstant(last));
+        long int val = math_obj_mvalue_getAsLong(last);
+        if (val == 1) {
+            math_obj_free(last);
+            self->children[indexOfFirst] = NULL;
+        }
+        else {
+            self->children[indexOfFirst] = last;
+        }
+        
+    }
+
+    self->children = __math_obj_takeOutNull(self->children);
+
+    if (len(self->children) == 1) {
+        // only one left
+        math_obj tmp = self->children[0];
+        self->children[0] = NULL;
+        math_obj_free(self);
+        return tmp;
+    }
+    else if (len(self->children) == 0) {
+        // nothing left
+        math_obj tmp = buildMathObjectConstantLong(1);
+        math_obj_free(self);
+        return tmp;
+    }
+
+    return self;
+}
+
 math_obj math_obj_eval(math_obj self) {
     // if not returning self, free self!!!
-
     assert(self != NULL);
 
     int childCount = len(self->children);
+
+    for (int i = 0; i < childCount; ++i) {
+        self->children[i] = math_obj_eval(self->children[i]);
+    }
 
     if (childCount == 0) {
         return self; // cant simplify one thing
     }
     elif (childCount == 1) {
-        self->children[0] = math_obj_eval(self->children[0]);
-
         switch (self->typeTag)
         {
         case NEGATE:
-            assert(childCount == 1);
-
-            if (math_obj_isConstant(self->children[0])) {
-                math_obj newSelf = __math_obj_eval_negate(self->children[0]);
-
-                math_obj_free(self);
-                return newSelf;
-            }
-
-            return self;
-            break;
-        
+            return math_obj_eval_negate(self);        
         default:
             assert(false);
             break;
@@ -167,143 +282,17 @@ math_obj math_obj_eval(math_obj self) {
         return self; // don't have any of these yet
     }
     else {
-        for (int i = 0; i < childCount; ++i) {
-            self->children[i] = math_obj_eval(self->children[i]);
-        }
-
         switch (self->typeTag)
         {
         case EQUATION:
             return self;
+        case PLUS: 
+            return math_obj_eval_plus(self);
+        case PRODUCT:
+            return math_obj_eval_product(self);
+        case FRACTION:
+            throw_error("Not Implemented", "FRACTION");
             break;
-        case PLUS: {    
-                assert(childCount > 1);
-                // analyze for possible simplification
-                math_obj last = NULL;
-                int indexOfFirst = -1;
-
-                for (int i = 0; i < childCount; ++i) {
-                    if (math_obj_isConstant(self->children[i])) {
-                        if (last == NULL) {
-                            last = self->children[i];
-                            self->children[i] = NULL;
-                            indexOfFirst = i;
-                        }
-                        else {
-                            math_obj newlast = __math_obj_eval_plus(last, self->children[i]);
-                            math_obj_free(last);
-                            math_obj_free(self->children[i]);
-                            self->children[i] = NULL;
-
-                            last = newlast;
-                        }
-                    }
-                }
-
-                if (last != NULL) {
-                    assert(math_obj_isConstant(last));
-                    long int val = math_obj_mvalue_getAsLong(last);
-                    if (val == 0) {
-                        math_obj_free(last);
-                        self->children[indexOfFirst] = NULL;
-                    }
-                    else {
-                        self->children[indexOfFirst] = last;
-                    }
-                    
-                }
-
-                self->children = __math_obj_takeOutNull(self->children);
-
-                if (len(self->children) == 1) {
-                    // only one left
-                    math_obj tmp = self->children[0];
-                    self->children[0] = NULL;
-                    math_obj_free(self);
-                    return tmp;
-                }
-                else if (len(self->children) == 0) {
-                    // nothing left
-                    math_obj tmp = buildMathObjectConstantLong(0);
-                    math_obj_free(self);
-                    return tmp;
-                }
-
-                return self;
-                break;
-            }
-        
-        case PRODUCT: {
-                assert(childCount > 1);
-                // analyze for possible simplification
-                math_obj last = NULL;
-                int indexOfFirst = -1;
-
-                for (int i = 0; i < childCount; ++i) {
-                    if (math_obj_isConstant(self->children[i])) {
-
-                        long int val = math_obj_mvalue_getAsLong(self->children[i]);
-                        
-                        if (val == 0) {
-                            // everything is zero
-                            math_obj_free(self);
-                            return buildMathObjectConstantLong(0);
-                        }
-
-                        if (last == NULL) {
-                            last = self->children[i];
-                            self->children[i] = NULL;
-                            indexOfFirst = i;
-                        }
-                        else {
-                            math_obj newlast = __math_obj_eval_product(last, self->children[i]);
-                            math_obj_free(last);
-                            math_obj_free(self->children[i]);
-                            self->children[i] = NULL;
-
-                            last = newlast;
-                        }
-                    }
-                }
-
-                if (last != NULL) {
-                    assert(math_obj_isConstant(last));
-                    long int val = math_obj_mvalue_getAsLong(last);
-                    if (val == 1) {
-                        math_obj_free(last);
-                        self->children[indexOfFirst] = NULL;
-                    }
-                    else {
-                        self->children[indexOfFirst] = last;
-                    }
-                    
-                }
-
-                self->children = __math_obj_takeOutNull(self->children);
-
-                if (len(self->children) == 1) {
-                    // only one left
-                    math_obj tmp = self->children[0];
-                    self->children[0] = NULL;
-                    math_obj_free(self);
-                    return tmp;
-                }
-                else if (len(self->children) == 0) {
-                    // nothing left
-                    math_obj tmp = buildMathObjectConstantLong(1);
-                    math_obj_free(self);
-                    return tmp;
-                }
-
-                return self;
-                break;
-            }
-        
-        case FRACTION: {
-                throw_error("Not Implemented", "FRACTION");
-            }
-
-        
         default:
             assert(false);
             break;
